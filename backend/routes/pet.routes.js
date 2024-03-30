7cf1214b6e042b491cc23c452c9ee95eeb18e4fa
heyyrahul <rkkamat9213@gmail.com>
2024-03-31 04:03:40 +0530

    pet-GetRoute-with-Id

--- backend/routes/pet.routes.js
+++ backend/routes/pet.routes.js
@@ -3,53 +3,60 @@ const { PetModel } = require("../model/pet.model");
 
 const petRouter = express.Router();
 
-petRouter.get("/",async(req,res)=>{
-    try{
-        const pet = await PetModel.find();
-		res.status(200).json({pet});
+petRouter.get("/", async (req, res) => {
+    try {
+        const pets = await PetModel.find();
+        res.status(200).json({ pets });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
     }
-    catch(e){
-        res.status(400).json({err})
-    }
-})
+});
 
-petRouter.post("/",async(req,res)=>{
-    const payload = req.body
-    try{
-        const pet = new PetModel(payload)
-        await pet.save()
-		res.status(200).json({msg:"data has been added"});
+petRouter.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const pet = await PetModel.findById(id);
+        if (!pet) {
+            return res.status(404).json({ message: "Pet not found" });
+        }
+        res.status(200).json({ pet });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
     }
-    catch(e){
-        res.status(400).json({e})
-    }
-})
+});
 
-petRouter.patch("/:id",async(req,res)=>{
-    const { id } =req.params
-    const payload = req.body
-    try{
-        await PetModel.findByIdAndUpdate(id,payload,{new:true})
-		res.status(200).json({msg:"data has been updated"});
+petRouter.post("/", async (req, res) => {
+    const payload = req.body;
+    try {
+        const pet = new PetModel(payload);
+        await pet.save();
+        res.status(200).json({ msg: "Data has been added" });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
     }
-    catch(e){
-        res.status(400).json({e})
-    }
-})
+});
 
-petRouter.delete("/:id",async(req,res)=>{
-    const { id } = req.params
-    
-    try{
-        await PetModel.findByIdAndDelete(id,{new:true})
-		res.status(200).json({msg:"data has been deleted"});
+petRouter.patch("/:id", async (req, res) => {
+    const { id } = req.params;
+    const payload = req.body;
+    try {
+        await PetModel.findByIdAndUpdate(id, payload, { new: true });
+        res.status(200).json({ msg: "Data has been updated" });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
     }
-    catch(e){
-        res.status(400).json({e})
-    }
-})
+});
 
+petRouter.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await PetModel.findByIdAndDelete(id);
+        res.status(200).json({ msg: "Data has been deleted" });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+});
 
 module.exports = {
     petRouter
-}
+};
