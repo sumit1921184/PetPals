7f8f7144bb9a097dc7a2d60f760f8f5a041b0fdc
anshukumari181405 <146926520+anshukumari181405@users.noreply.github.com>
2024-04-03 13:33:21 +0530

    merge

--- frontend/src/Routing/AllRoutes.jsx
+++ frontend/src/Routing/AllRoutes.jsx
@@ -1,14 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-<<<<<<< HEAD
-=======
 
-
-
-import { Route, Routes } from 'react-router-dom';
-
-
->>>>>>> c20377c005738e0b17301c1ea35ca7a6eb7076ee
 import Homepage from "../Pages/HomePage";
 import Contact from "../Pages/Contact";
 import { LoginForm } from "../Pages/Login";
@@ -16,10 +8,6 @@ import About from "../Pages/About";
 import SignUp from "../components/SignUp";
 import Adopt from "../Pages/Adopt";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c20377c005738e0b17301c1ea35ca7a6eb7076ee
 function Allroutes() {
   return (
     <Routes>
