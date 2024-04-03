74fd644750bb63493ffc58789b7baae8b6b17a78
anshukumari181405 <146926520+anshukumari181405@users.noreply.github.com>
2024-04-03 13:28:39 +0530

    review

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@
 
 import Article from "../Components/Article";
 import FirstDiv from "../Components/FirstDiv";
+import Review from "../Components/Review";
 
 
 
@@ -14,9 +15,13 @@ const HomePage = () => {
     <div>
       
       <FirstDiv/>
-      <h2 style={{ textAlign: 'center',fontSize: '50px', margin: '20px 0',fontWeight: 'bold',fontFamily: "sans-serif", color:'orange'}}>You can’t buy love, but you can rescue it</h2>
+      <h2 style={{ textAlign: 'center',fontSize: '50px', margin: '20px 0',fontWeight: 'bold',fontFamily: "sans-serif", color:'orange'}}>You cant buy love, but you can rescue it</h2>
       <Article/>
+      <h2 style={{ textAlign: 'center',fontSize: '30px', margin: '20px 0',fontWeight: 'bold',fontFamily: "sans-serif", color:'orange'}}>Review</h2>
+
+      <Review/>
       
+
       
     </div>
   );
